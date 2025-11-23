877e1b8853e5f6b48030f9a6b81b36887924b780
thom0901 <thomsniekers123@gmail.com>
2025-11-23 11:14:31 +0100

    dildo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcam.java
@@ -22,7 +22,7 @@ public class AprilTagWebcam {
     private Telemetry telemetry;
 
     public void init(HardwareMap hwMap, Telemetry telemetry) {
-        this.telemetry= telemetry;
+        this.telemetry = telemetry;
         aprilTagProcessor = new AprilTagProcessor.Builder()
                 .setDrawTagID(true)
                 .setDrawTagOutline(true)
@@ -42,3 +42,37 @@ public class AprilTagWebcam {
     public void update() {
         detectedTags = aprilTagProcessor.getDetections();
     }
+    
+    public List<AprilTagDetection> getDetectedTags() {
+        return detectedTags;
+    }  
+    
+    public void displayDetectionTelemetry(AprilTagDetection detectedId){
+     if (detectedId != null) {return;}
+        if (detectedId.metadata != null) {
+            telemetry.addLine(String.format("\n==== (ID %d) %s", detectedId.id, detectedId.metadata.name));
+            telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detectedId.pose.x, detectedId.pose.y, detectedId.pose.z));
+            telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detectedId.pose.pitch, detectedId.pose.roll, detectedId.pose.yaw));
+            telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detectedId.pose.range, detectedId.pose.bearing, detectedId.pose.elevation));
+        } else {
+            telemetry.addLine(String.format("\n==== (ID %d) Unknown", detectedId.id));
+            telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detectedId.center.x, detectedId.center.y));
+        }
+    }
+}
+    
+    public AprilTagDetection getTagBySpecificId(int id){
+        for (AprilTagDetection detection : detectedTags) {
+            if (detection.id == id) {
+                return detection;
+            }
+        }
+        return null;
+    
+    }
+    
+    public void stop() {
+        if (visionPortal != null) {
+            visionPortal.close();
+    }
+}
